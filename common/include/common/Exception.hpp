73a10aa89a72849ee591cedece55f8d3f6efecfc
younghwan.park <pfpyh00@gmail.com>
2026-01-24 08:42:42 +0000

    common: project tree change
    [Major]
    To support extension of common-lib, folder tree changed.
    External libs are move to submodules.
    
    task: COMMO-35

---
+++ common/include/common/Exception.hpp
@@ -0,0 +1,108 @@
+/**********************************************************************
+MIT License
+
+Copyright (c) 2025 Park Younghwan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+**********************************************************************/
+
+#pragma once
+
+#include "common/CommonHeader.hpp"
+
+#include <exception>
+#include <string>
+
+namespace common
+{
+class COMMON_LIB_API BaseException : public std::exception
+{
+protected :
+    std::string _detail;
+
+public :
+    BaseException() = default;
+    BaseException(const std::string& detail)
+        : _detail(detail) { }
+};
+
+class COMMON_LIB_API AlreadyRunningException : public BaseException
+{
+public :
+    AlreadyRunningException() = default;
+    AlreadyRunningException(const std::string& detail)
+        : BaseException(detail) { }
+
+public :
+    const char* what() const noexcept override
+    {
+        const std::string what = _detail.empty() ? 
+            "already running" : "already running:" + _detail;
+        return what.c_str();
+    }
+};
+
+class COMMON_LIB_API BadHandlingException : public BaseException
+{
+public :
+    BadHandlingException() = default;
+    BadHandlingException(const std::string& detail)
+        : BaseException(detail) { }
+
+public :
+    const char* what() const noexcept override
+    {
+        const std::string what = _detail.empty() ? 
+            "bad handling" : "bad handling:" + _detail;
+        return what.c_str();
+    }
+};
+
+class COMMON_LIB_API OutOfRangeException : public BaseException
+{
+public :
+    OutOfRangeException() = default;
+    OutOfRangeException(const std::string& detail)
+        : BaseException(detail) { }
+
+public :
+    const char* what() const noexcept override
+    {
+        const std::string what = _detail.empty() ? 
+            "out of range" : "out of range:" + _detail;
+        return what.c_str();
+    }
+};
+
+class COMMON_LIB_API CommunicationException : public BaseException
+{
+public :
+    CommunicationException() = default;
+    CommunicationException(const std::string& detail)
+        : BaseException(detail) { }
+
+public :
+    const char* what() const noexcept override
+    {
+        const std::string what = _detail.empty() ? 
+            "communication error" : "communication error:" + _detail;
+        return what.c_str();
+    }
+};
+} // namespace common
---
+++ extension/math/include/common/extension/math/filter/LowPassFilter.hpp
@@ -0,0 +1,54 @@
+/**********************************************************************
+MIT License
+
+Copyright (c) 2025 Park Younghwan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+**********************************************************************/
+
+#pragma once
+
+#include "common/CommonHeader.hpp"
+
+namespace common::math
+{
+template <typename T>
+class COMMON_LIB_API LowPassFilter
+{
+private:
+    T _prev;
+    T _alpha ;
+    bool _first = true;
+
+public:
+    LowPassFilter(const T& init, const T& alpha)
+        : _prev(init), _alpha(alpha) {};
+
+    auto run(const T& x)->T
+    {
+        if (_first)
+        {
+            _prev = x;
+            _first = false;
+        }
+        _prev = _alpha * _prev + (1 - _alpha) * x;
+        return _prev;
+    };
+};
+} // namespace common::math
